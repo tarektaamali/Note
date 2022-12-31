a1672050c6c313359ed4d70f20afd32471d374b2
Tarek.Taamali <tarek.taamali@inetum.com>
2022-12-31 09:20:59 +0100

    Added a throw exception in onPressed function to test crashlytics integration.

--- lib/main.dart
+++ lib/main.dart
@@ -125,7 +125,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () => throw Exception(),
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
